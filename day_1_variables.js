08136e5bb6edcfc6c52969fe98697650d7b30425
Samrat Neupane <71063668+SamratNeupane787@users.noreply.github.com>
2024-04-21 20:52:36 +0545

    first commit const/let/var

---
+++ day_1_variables.js
@@ -0,0 +1,14 @@
+//const is defined only once , which is not to be changed daily//
+
+const accId = 1122e1;
+let accEmail = "sdkf@gmail.com";
+var accPas = "1234";
+accountCity = "kathmandu";
+
+// accId = 2;
+console.log(accId);
+console.table([accEmail, accId, accPas, accountCity]);
+
+//prefer not to use var because of issue in block scope//
+
+//if we just define a variable then the value of the varibale will be undefined//
