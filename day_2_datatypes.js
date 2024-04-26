d5c42c29af5ac12953856d7b441de19dda38040e
Samrat Neupane <71063668+SamratNeupane787@users.noreply.github.com>
2024-04-26 21:01:13 +0545

    datatypes and variables revised

---
+++ day_2_datatypes.js
@@ -0,0 +1,17 @@
+"use strict"; // treat all JS code in newer js
+// alert("hellow!!"); // only in browser not in node js
+
+let name = "samrat";
+let age = 18;
+let faksd = true;
+
+// number
+//bigInt
+//string
+//boolean
+//null   => standalone value
+//undefined =>
+//symbol => uniqueness we talk about symbol
+
+//object
+console.log(typeof age);
---
+++ day_3_variables.js
@@ -0,0 +1,25 @@
+// let score = 33;
+
+// let score = "33";
+
+// let valueInNumber = Number(score);
+
+// console.log(typeof valueInNumber);
+
+let scoree = "33aavb";
+
+let valueData = Number(scoree);
+
+console.log(typeof valueData);
+
+//but the value of valueData is NaN (NOT A NUMEBR)
+console.log(valueData); //==> NaN
+
+//All capital letter in first
+//Number()
+//String()
+//Boolean()
+
+//1 ==> true , 0 ==> false
+// "" = > false
+// "samrat" => true
